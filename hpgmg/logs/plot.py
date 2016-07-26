6365d57dc0c30ad725193aaca9d37ddd4ce3c93c
Steve <sdalton1@gmail.com>
2016-07-26 10:13:47 -0700

    Updated plotting code

--- hpgmg/logs/plot.py
+++ hpgmg/logs/plot.py
@@ -1,11 +1,23 @@
 import re
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 from StringIO import StringIO
+from matplotlib import rc
+
+rc('font', **{'family':'serif', 'serif':['Computer Modern']})
+rc('text', usetex=True);
+fontsize = 18;
+family = 'sans-serif';
+
+def save_plot(pdfname):
+    plt.savefig(pdfname)
+    os.system('pdfcrop {0} {0}'.format(pdfname)); # whitespace crop
+    os.system('pdfcrop --margins \'0 2 0 0\' {0} {0}'.format(pdfname)); # top tickmark crop
 
 filename_template = 'hpgmg-fv-{}.log';
 
-names = ['openmp', 'agency'];
+names = ['openmp', 'agency', 'agency-openmp'];
 data  = {};
 
 for name in names:
@@ -16,10 +28,19 @@ for name in names:
         stripblock = re.sub(r'([A-Za-z])\s([A-Za-z])', r'\1\2', block);
 
         df = pd.read_table(StringIO(stripblock), delim_whitespace=True, header=None);
-        df.columns = ['Operation'] + ['level{}'.format(i) for i in range(len(df.columns) - 1)];
+        df.columns = ['Level'] + ['{}'.format(i) for i in range(len(df.columns) - 2)] + ['Total'];
+        df = df.set_index('Level');
         data[name] = df.T;
 
-# data[names[1]].iloc[2:-1][1:-1].plot(style='-o');
-print data[names[0]][0];
-# plt.plot(df[names[0]].BLAS1);
-# plt.show();
+        columns=['smooth', 'residual', 'BLAS1', 'BoundaryConditions', 'Restriction', 'Interpolation', 'GhostZoneExchange'];
+        M = data[name].as_matrix(columns);
+
+        plt.figure();
+        lineObjects = plt.semilogy(M[:-1,:] * 100, '-o');
+        plt.ylabel('Level');
+        plt.ylabel('Time (ms)');
+        plt.legend(iter(lineObjects), columns, loc='lower center', bbox_to_anchor=(0.5, -0.10), ncol=4, fancybox=True, shadow=True);
+        plt.title(name);
+        save_plot('{}_figure.pdf'.format(name));
+
+plt.show();
